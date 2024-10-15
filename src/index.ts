f9a3162c2a2fec90bba67fe7426e5fb4683ff03e
Pavel Arlou <arloupav@gmail.com>
2024-10-15 22:36:25 +0300

    fix: json

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express  from 'express';
 
 const app = express();
 
-const port=process.env.PORT||3000
+const port=process.env.PORT||3000;
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello!!')
 });
 
 app.listen(port);
