f9bb69303125d3bf20cb3fb85502889e3014949c
Pavel Arlou <arloupav@gmail.com>
2024-10-15 22:19:14 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express  from 'express';
 
 const app = express();
 
+const port=process.env.PORT||3000
 app.get('/', function (req, res) {
     res.send('Hello World')
 });
 
-app.listen(3000);
+app.listen(port);
